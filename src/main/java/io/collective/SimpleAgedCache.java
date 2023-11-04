0c672904e65e12d59f45d151de1f4cf4625e32da
Rachael Ferguson <fergusonrae@outlook.com>
2023-11-04 23:31:33 +0000

    Code out functions so tests pass.

--- src/main/java/io/collective/SimpleAgedCache.java
+++ src/main/java/io/collective/SimpleAgedCache.java
@@ -1,27 +1,72 @@
 package io.collective;
 
 import java.time.Clock;
+import java.time.Instant;
 
 public class SimpleAgedCache {
+    private static final int MAX_ENTRIES = 10;
+    private ExpirableEntry[] entries = new ExpirableEntry[MAX_ENTRIES];
+    private Clock clock = Clock.systemDefaultZone();
 
     public SimpleAgedCache(Clock clock) {
+        this.clock = clock;
     }
 
     public SimpleAgedCache() {
     }
 
     public void put(Object key, Object value, int retentionInMillis) {
+        for (int i = 0; i < entries.length; i ++)
+            if (entries[i] == null) {
+                entries[i] = new ExpirableEntry(key, value, retentionInMillis);
+                break;
+            }
     }
 
+
     public boolean isEmpty() {
-        return false;
+        removeExpired();
+        int counter = 0;
+        for (int i = 0; i < entries.length; i ++)
+            if (entries[i] != null)
+                counter ++;
+        return counter == 0;
     }
 
     public int size() {
-        return 0;
+        removeExpired();
+        int counter = 0;
+        for (int i = 0; i < entries.length; i ++)
+            if (entries[i] != null)
+                counter ++;
+        return counter;
     }
 
     public Object get(Object key) {
+        removeExpired();
+        for (int i = 0; i < entries.length; i ++)
+            if ((entries[i] != null) && (entries[i].key == key))
+                return entries[i].value;
         return null;
+    }
+
+    public void removeExpired() {
+        Instant currentInstant = clock.instant();
+        for (int i = 0; i < entries.length; i ++) {
+            if ((entries[i] != null) && (entries[i].expirationInstant.isBefore(currentInstant)))
+                entries[i] = null;
+        }
+    }
+
+    private class ExpirableEntry {
+        private Object key;
+        private Object value;
+        private Instant expirationInstant;
+
+        private ExpirableEntry(Object key, Object value, int retentionInMillis) {
+            this.key = key;
+            this.value = value;
+            this.expirationInstant = clock.instant().plusMillis(retentionInMillis);
+        }
     }
 }
